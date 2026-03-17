73d70088d03ca761c9d88ca331d1d116d94e5e94
Morgan Nguyen <>
2026-03-17 10:38:25 -0500

    Update possibleMoves for Bishop and Queen

--- pieces/Bishop.java
+++ pieces/Bishop.java
@@ -15,5 +15,39 @@ public class Bishop extends Piece{
 
         int row = position.getRow();
         int col = position.getCol();
+
+        // Move up and to the left
+        for(int i = 1; row - i >= 0 && col - 1 >= 0; i++) {
+            moves.add(new Position(row - i, col - i));
+            if(board[row - i][col - i] != null) {
+                break;
+            }
+        }
+        
+        // Move up and to the right
+        for(int i = 1; row - i >= 0 && col + 1 < 8; i++) {
+            moves.add(new Position(row - i, col + i));
+            if(board[row - i][col + i] != null) {
+                break;
+            }
+        }
+
+        // Move down and to the left
+        for(int i = 1; row + i < 8 && col - 1 >= 0; i++) {
+            moves.add(new Position(row + i, col - i));
+            if(board[row + i][col - i] != null) {
+                break;
+            }
+        }
+
+        // Move down and to the right
+        for(int i = 1; row + i < 8 && col + 1 < 8; i++) {
+            moves.add(new Position(row + i, col + i));
+            if(board[row + i][col + i] != null) {
+                break;
+            }
+        }
+
+        return moves;
     }
 }
--- pieces/Queen.java
+++ pieces/Queen.java
@@ -9,11 +9,78 @@ public class Queen extends Piece{
         super(color, position);
     }
 
+    // Moves are Rook and Bishop combined
     @Override
     public List<Position> possibleMoves(Piece[][] board) {
         List<Position> moves = new ArrayList<>();
 
         int row = position.getRow();
         int col = position.getCol();
+
+        // Moving up
+        for(int i = row - 1; i >= 0; i--) {
+            moves.add(new Position(i, col));
+            if(board[i][col] != null) {
+                break;
+            }
+        }
+
+        // Moving down
+        for(int i = row + 1; i < 8; i++) {
+            moves.add(new Position(i, col));
+            if(board[i][col] != null) {
+                break;
+            }
+        }
+
+        // Moving right
+        for(int i = col - 1; i >= 0; i--) {
+            moves.add(new Position(row, i));
+            if(board[row][i] != null) {
+                break;
+            }
+        }
+
+        // Moving left
+        for(int i = col + 1; i < 8; i++) {
+            moves.add(new Position(row, i));
+            if(board[row][i] != null) {
+                break;
+            }
+        }
+
+        // Move up and to the left
+        for(int i = 1; row - i >= 0 && col - 1 >= 0; i++) {
+            moves.add(new Position(row - i, col - i));
+            if(board[row - i][col - i] != null) {
+                break;
+            }
+        }
+        
+        // Move up and to the right
+        for(int i = 1; row - i >= 0 && col + 1 < 8; i++) {
+            moves.add(new Position(row - i, col + i));
+            if(board[row - i][col + i] != null) {
+                break;
+            }
+        }
+
+        // Move down and to the left
+        for(int i = 1; row + i < 8 && col - 1 >= 0; i++) {
+            moves.add(new Position(row + i, col - i));
+            if(board[row + i][col - i] != null) {
+                break;
+            }
+        }
+
+        // Move down and to the right
+        for(int i = 1; row + i < 8 && col + 1 < 8; i++) {
+            moves.add(new Position(row + i, col + i));
+            if(board[row + i][col + i] != null) {
+                break;
+            }
+        }
+
+        return moves;
     }
 }
