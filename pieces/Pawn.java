2a543cfd5f198167971a9520d88bee8005d1689a
Morgan Nguyen <>
2026-03-17 07:39:54 -0500

    Fixed valid boolean function

--- pieces/Pawn.java
+++ pieces/Pawn.java
@@ -14,6 +14,8 @@ public class Pawn extends Piece{
     private boolean valid(int row, int col) {
         if((row >= 0 && row < 8) && (col >= 0 && col <8)) {
             return true;
+        } else {
+            return false;
         }
     }
 
