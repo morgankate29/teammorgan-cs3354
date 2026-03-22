fa18e0c437064292d8b7e9873b29d2f6741a9244
Morgan Nguyen <>
2026-03-22 15:03:55 -0500

    Updated Player to delete unused imports and fixed array format

--- player/Player.java
+++ player/Player.java
@@ -2,17 +2,14 @@
 
 package player;
 import pieces.Piece;
-import board.Board;
-import utils.Position;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Player {
     private String name;
     private String color;
-    private ArrayList availablePieces;
+    private ArrayList<Piece> availablePieces;
 
     public Player(String name, String color) {
         this.name = name;
