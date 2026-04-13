fbf766ccb299feea764ee174d9efc0046004743b
Morgan Nguyen <>
2026-04-12 20:19:42 -0500

    Updated ChessFrame layout and added game argument

--- gui/ChessFrame.java
+++ gui/ChessFrame.java
@@ -7,6 +7,8 @@ package gui;
 import javax.swing.*;
 import java.awt.*;
 
+import game.Game;
+
 public class ChessFrame extends JFrame {
     /**
      * Constructor to initialize the window
@@ -15,11 +17,14 @@ public class ChessFrame extends JFrame {
      * Set a border layout
      * Makes the window visible
      */
-    public ChessFrame() {
+    public ChessFrame(Game game) {
         setTitle("Chess Game");
         setSize(1000, 1000);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
+
+        add(new ChessBoardPanel(game), BorderLayout.CENTER);
+
         setVisible(true);
     }
 }
