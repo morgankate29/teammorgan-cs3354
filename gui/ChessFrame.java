c613850ecd66ff00a9912cfe6e59aee5d078807e
Morgan Nguyen <>
2026-04-13 18:12:35 -0500

    Implemented Menu Bar, used AI to help minor syntax error

--- gui/ChessFrame.java
+++ gui/ChessFrame.java
@@ -23,8 +23,46 @@ public class ChessFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
+        ChessBoardPanel cbp = new ChessBoardPanel(game);
         add(new ChessBoardPanel(game), BorderLayout.CENTER);
 
+        setJMenuBar(createMenuBar(game, cbp));
+
         setVisible(true);
+    }
+
+    private JMenuBar createMenuBar(Game game, ChessBoardPanel cbp) {
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("File");
+
+        JMenuItem newGame = new JMenuItem("New Game");
+        JMenuItem saveGame = new JMenuItem("Save Game");
+        JMenuItem loadGame = new JMenuItem("Load Game");
+
+        fileMenu.add(newGame);
+        fileMenu.add(saveGame);
+        fileMenu.add(loadGame);
+
+        menuBar.add(fileMenu);
+
+        newGame.addActionListener(e -> {
+            game.reset();
+            cbp.refresh();
+        });
+
+        saveGame.addActionListener(e -> {
+            GameIO.save(game);
+            JOptionPane.showMessageDialog(this, "Game saved!");
+        });
+
+        loadGame.addActionListener(e -> {
+            Game loaded = GameIO.load();
+            game.copyFrom(loaded);
+            cbp.refresh();
+            JOptionPane.showMessageDialog(this, "Game loaded!");
+        });
+
+        return menuBar;
     }
 }
